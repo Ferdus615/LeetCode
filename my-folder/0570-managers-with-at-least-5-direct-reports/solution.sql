103666e86d11a773b2fad12a1e3b07516905d88a
Ferdus Rhaman <ferdus615@gmail.com>
2025-09-13 09:27:19 +0000

    Sync LeetCode submission Runtime - 284 ms (92.70%), Memory - 0.0B (100.00%)

--- my-folder/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ my-folder/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
-select e1.name
-from Employee e1
-inner join Employee e2
-on e1.id = e2.managerId 
-group by e1.id, e1.name, e1.managerId
-having count(e2.managerID) >= 5;
+select e.name
+from Employee e
+left join Employee m
+on e.id = m.managerID
+group by e.name, m.managerID
+having count(m.managerID) >= 5;
