6c209ff7d57ebd9415c3b9772e0f8ffb63561d0a
Ferdus Rhaman <ferdus615@gmail.com>
2025-09-17 04:18:33 +0000

    Sync LeetCode submission Runtime - 33 ms (93.58%), Memory - 53.2 MB (80.84%)

---
+++ my-folder/2796-allow-one-function-call/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+
+    let callCount = true;
+    return function (...args) {
+        return callCount ? (callCount = false, fn(...args)) : undefined;
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
