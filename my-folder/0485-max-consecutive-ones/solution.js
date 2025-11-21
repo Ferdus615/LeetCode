3eebdff488f6aea0a8dfe642033daffbba1b39c5
Ferdus Rhaman <ferdus615@gmail.com>
2025-11-21 15:06:17 +0000

    Sync LeetCode submission Runtime - 2 ms (71.74%), Memory - 59.2 MB (79.23%)

---
+++ my-folder/0485-max-consecutive-ones/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let count = 0;
+    let result = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            if (count > result) result = count;
+            count = 0;
+        } else {
+            count ++;
+        }
+    }
+
+    return count > result ? count : result;
+};
