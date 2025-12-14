7d89e39db00e461e73ce9a7c44ffbc32fc4025f0
Ferdus Rhaman <ferdus615@gmail.com>
2025-12-14 16:18:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 58.2 MB (43.24%)

---
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = 0;
+    
+    for (let i=1; i<nums.length; i++) {
+        if (nums[i] !== nums[index]) {
+            nums[index + 1] = nums[i];
+            index++;
+        } else {
+            nums[i] = 0;
+        }
+    }
+    return index+1;
+};
