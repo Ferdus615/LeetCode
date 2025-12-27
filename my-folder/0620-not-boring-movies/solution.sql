421ca93f6385a438f9a60412bb4433ef67d67962
Ferdus Rhaman <ferdus615@gmail.com>
2025-12-27 16:50:18 +0000

    Sync LeetCode submission Runtime - 170 ms (97.12%), Memory - 0.0B (100.00%)

--- my-folder/0620-not-boring-movies/solution.sql
+++ my-folder/0620-not-boring-movies/solution.sql
@@ -1,6 +1,5 @@
 -- Write your PostgreSQL query statement below
 select id, movie, description, rating
 from Cinema
-where id % 2 != 0
-and description != 'boring'
+where id % 2 = 1 and description != 'boring'
 order by rating desc
