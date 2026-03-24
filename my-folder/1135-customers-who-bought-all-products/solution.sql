6a4d8dc55cfc213930820584e58d8a3daea93249
Ferdus Rhaman <ferdus615@gmail.com>
2026-03-24 18:32:00 +0000

    Sync LeetCode submission Runtime - 474 ms (90.43%), Memory - 0.0B (100.00%)

--- my-folder/1135-customers-who-bought-all-products/solution.sql
+++ my-folder/1135-customers-who-bought-all-products/solution.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-select c.customer_id from Customer c
-group by c.customer_id
+select customer_id from Customer
+group by customer_id
 having count(distinct product_key) = (select count(*) from Product)
